6ba724f216a23506c12c39002f5594916813b3bc
Nayumi563 <aimie.ad6@gmail.com>
2026-09-12 03:40:15 +0200

    bug fix activate order in pile

--- Assets/Scripts/PileContainer.cs
+++ Assets/Scripts/PileContainer.cs
@@ -4,45 +4,65 @@ using UnityEngine.UI;
 public class PileContainer : ContainerCard {
     private int _count = -1;
     [SerializeField] private Button _button;
+
     public override void AddElement(CardElement card) {
         base.AddElement(card);
         card.Info.SetIsReturn(false);
     }
 
     public override void RemoveElement(CardElement card) {
-        if (CardElements.IndexOf(card) > 1) {
-            CardElements[CardElements.IndexOf(card) - 2].gameObject.SetActive(true);
-            CardElements[CardElements.IndexOf(card) - 2].transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-
-            CardElements[CardElements.IndexOf(card) - 1].transform.position = new Vector3(transform.position.x, transform.position.y, -1);
-        }
-        _count--;
         base.RemoveElement(card);
 
         if (CardElements.Count == 0)
         {
             _button.gameObject.SetActive(false);
+            return;
         }
+
+        _count--;
+        UpdatePile();
     }
 
     private void Start() {
-        foreach (CardElement card in CardElements) {
+        foreach (CardElement card in CardElements) 
+        {
             card.gameObject.SetActive(false);
         }
     }
 
     public void OnClickPile() {
-        if (_count >= 1) 
-        {
-            CardElements[(_count - 1) % CardElements.Count].gameObject.SetActive(false);
-        }
-        if (_count >= 0) 
-        {
-            CardElements[_count % CardElements.Count].transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-        }
         _count++;
-        CardElements[_count % CardElements.Count].gameObject.SetActive(true);
-        CardElements[_count % CardElements.Count].transform.position = new Vector3(transform.position.x, transform.position.y, -1);
+        UpdatePile();
     }
 
+    private void UpdatePile()
+    {
+        if (_count >= CardElements.Count)
+        {
+            _count = -1;
+
+            foreach (CardElement card in CardElements)
+            {
+                card.gameObject.SetActive(false);
+            }
+            return;
+        }
+
+        if (_count > 1)
+        {
+            CardElements[(_count - 2)].gameObject.SetActive(false);
+        }
+
+        if (_count > 0)
+        {
+            CardElements[(_count - 1)].gameObject.SetActive(true);
+            CardElements[(_count - 1)].transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        
+        if (_count >= 0)
+        {
+            CardElements[_count].gameObject.SetActive(true);
+            CardElements[_count].transform.position = new Vector3(transform.position.x, transform.position.y, -1);
+        }
+    }
 }
