bdc485b8520602c9ca5e40e7fb7a566a8df6216d
Nayumi563 <aimie.ad6@gmail.com>
2026-09-14 22:40:25 +0200

    bug fix storage container condition + refacto

--- Assets/Scripts/StorageContainer.cs
+++ Assets/Scripts/StorageContainer.cs
@@ -1,8 +1,12 @@
-﻿public class StorageContainer : ContainerCard
+﻿using UnityEngine;
+
+public class StorageContainer : ContainerCard
 {
     public override bool AddCard(CardElement element)
     {
-        if (CardIsValid(element))
+        CardElement[] childrenCards = element.GetComponentsInChildren<CardElement>();
+
+        if (CardElements.Count - 1 < CardManager.Instance.CardNumbers.Length && element.Info.Number - 1 == CardElements.Count && childrenCards.Length == 1 && (CardElements.Count == 0 || CardElements[CardElements.Count - 1].Info.Category == element.Info.Category))
         {
             base.AddElement(element);
             element.gameObject.layer = 2;
@@ -14,24 +18,6 @@
             return true;
         }
         else
-        {
-            return false;
-        }
-    }
-
-    private bool CardIsValid(CardElement element) {
-        if (CardElements.Count - 1 < CardManager.Instance.CardNumbers.Length && element.Info.Number - 1 == CardElements.Count)
-        {
-            if (CardElements.Count == 0 || CardElements[CardElements.Count - 1].Info.Category == element.Info.Category) 
-            {
-                return true;
-            }
-            else 
-            {
-                return false;
-            }
-        }
-        else 
         {
             return false;
         }
